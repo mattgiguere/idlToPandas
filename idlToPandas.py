414e47fcf34d8a2643661a4dce572c849ac56171
Matt Giguere <matthew.giguere@gmail.com>
2014-10-15 18:36:57 -0400

    added endian fix
    added the big-endian/little-endian fix that is discussed in the
    associated IPython notebook

--- idlToPandas.py
+++ idlToPandas.py
@@ -51,6 +51,14 @@ def idlToPandas(fileName, keyValue=0):
     for tag in struct.dtype.descr:
         tags.append(tag[0][0])
 
+    #now take care of potential big-endian/little-endian issues
+    dt = struct.dtype
+    dt = dt.descr
+    for i in range(len(dt)):
+        if(dt[i][1][0] == '>' or dt[i][1][0] == '<'):
+            dt[i] = (dt[i][0], dt[i][1][1:])
+    struct = struct.astype(dt)
+
     pdf = pd.DataFrame.from_records(struct, columns=tags)
     return pdf
 
